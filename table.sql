e0992fa4d358783c02b9a01474bef10863745784
macconext <47002320+macconext@users.noreply.github.com>
2019-01-26 19:39:37 +0100

    create table.sql

---
+++ table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `tbladmin` (
+  `adminID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`adminID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
